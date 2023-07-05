e0611b3bd9b6ef8be2a5baf6d43fa7c1255d772b
gideonekekeke <gideonekeke64@gmail.com>
2023-07-05 18:28:53 +0100

    tabs

---
+++ conf.py
@@ -0,0 +1,7 @@
+extensions = ['sphinx_tabs.tabs']
+sphinx_tabs_valid_builders = ['linkcheck']
+
+sphinx_tabs_disable_tab_closing = True
+
+def setup(app):
+   app.add_lexer('alias', MyCustomLexer())
